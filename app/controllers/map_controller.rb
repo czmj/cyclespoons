37b374fb6fac45a866ee6c90b70ac74f1b9ce5ea
czmj <cj@theblackeyeproject.co.uk>
2017-07-27 15:53:23 +0100

    split pub data into helper

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,55 +1,8 @@
 class MapController < ApplicationController
+    include PubHelper
+
     def index
         @pubs = get_pubs
-        @markers = get_markers
-    end
-
-    private
-
-    def get_raw_data
-        require 'net/https'
-        
-        url = URI("https://www.jdwetherspoon.com/api/advancedsearch")
-
-        http = Net::HTTP.new(url.host, url.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        request = Net::HTTP::Post.new(url)
-        request["content-type"] = 'application/json'
-        request.body = "{\"region\":null,\"paging\":{\"UsePagination\":false},\"facilities\":[],\"searchType\":0}"
-
-        response = http.request(request)
-        return JSON.parse(response.read_body)
-    end
-
-    def get_pubs
-        pubs = Array.new
-        regions = get_raw_data['regions']
-
-        regions.each do |region|
-            region['subRegions'].each do |subregion|
-                subregion['items'].each do |pub|
-                    unless pub['PubIsTemporaryClosed'] || pub['PubIsClosed']
-                        pubs.push(pub)
-                    end
-                end
-            end
-        end
-        
-        return pubs;
-    end
-
-    def get_markers
-        markers = Array.new
-        pubs = @pubs || get_pubs
-
-        pubs.each do |pub|
-            markers.push({
-                :latlng => [pub['lat'], pub['lng']],
-                :popup => "<p><a href='//www.jdwetherspoon.com#{pub['url']}'>#{pub['name']}</a></p><p>#{pub['address1']}<br>#{pub['city']}<br>#{pub['postcode']}</p>"
-            })
-        end
-        return markers
+        @markers = get_markers(@pubs)
     end
 end
---
+++ app/helpers/pub_helper.rb
@@ -0,0 +1,50 @@
+module PubHelper
+    require 'net/https'
+
+    def get_pubs
+        pubs = Array.new
+        regions = get_raw_data['regions']
+
+        regions.each do |region|
+            region['subRegions'].each do |subregion|
+                subregion['items'].each do |pub|
+                    unless pub['PubIsTemporaryClosed'] || pub['PubIsClosed']
+                        pubs.push(pub)
+                    end
+                end
+            end
+        end
+        
+        return pubs;
+    end
+
+    def get_markers(pubs = get_pubs)
+        markers = Array.new
+
+        pubs.each do |pub|
+            markers.push({
+                :latlng => [pub['lat'], pub['lng']],
+                :popup => "<p><a href='//www.jdwetherspoon.com#{pub['url']}'>#{pub['name']}</a></p><p>#{pub['address1']}<br>#{pub['city']}<br>#{pub['postcode']}</p>"
+            })
+        end
+        return markers
+    end
+
+    private 
+
+    def get_raw_data
+        
+        url = URI("https://www.jdwetherspoon.com/api/advancedsearch")
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        request = Net::HTTP::Post.new(url)
+        request["content-type"] = 'application/json'
+        request.body = "{\"region\":null,\"paging\":{\"UsePagination\":false},\"facilities\":[],\"searchType\":0}"
+
+        response = http.request(request)
+        return JSON.parse(response.read_body)
+    end
+end
