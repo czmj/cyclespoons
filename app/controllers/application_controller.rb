75e1cd5aac353d47355b71db85b05b09f82451ae
czmj <clarazmjordan@gmail.com>
2017-08-13 23:09:30 +0100

    more images; strava api setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  @client = Strava::Api::V3::Client.new(:access_token => ENV['STRAVA_ACCESS_TOKEN'])
 end
