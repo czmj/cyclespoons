dcafad32c0c3cfde97416b7b7e06aa5453dfafa0
czmj <cj@theblackeyeproject.co.uk>
2017-08-14 14:00:52 +0100

    strava auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :strava_auth
 
-  @client = Strava::Api::V3::Client.new(:access_token => ENV['STRAVA_ACCESS_TOKEN'])
+  def strava_auth
+    code = strava_code
+
+    if code
+      access_information = Strava::Api::V3::Auth.retrieve_access(ENV['STRAVA_CLIENT_ID'], ENV['STRAVA_CLIENT_SECRET'], code)
+      access_token = access_information['access_token']
+      athlete_information = access_information['athlete']
+    end
+  end
+
+  private
+
+  def strava_code
+    if params[:code]
+      code = params[:code]
+      cookies.permanent[:strava_code] = code
+    else
+      code = cookies[:strava_code]
+    end
+
+    return code
+  end
 end
