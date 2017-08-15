68415ad5d0e851df26adb04d7a583c26102a0c7c
czmj <cj@theblackeyeproject.co.uk>
2017-08-15 15:34:19 +0100

    try moving plugins to head

--- app/assets/javascripts/application-head.js
+++ app/assets/javascripts/application-head.js
@@ -1,6 +1,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require leaflet
+//= require leaflet.geosearch
+//= require leaflet.geosearch.provider.openstreetmap
+//= require leaflet.polylineencoded
 
 //= require_self
 //= require_tree ./application-head
