2c37aca00313dd2af85754f7a9fff3b70b64e4a2
czmj <cj@theblackeyeproject.co.uk>
2017-08-15 11:24:23 +0100

    refactoring

--- app/assets/javascripts/application-head/leaflet-control-github.js
+++ app/assets/javascripts/application-head/leaflet-control-github.js
@@ -1,27 +1,28 @@
-
 L.Control.GitHub = L.Control.extend({
-options: {
-    position: 'topright'
-  },
-  initialize: function (options) {
-    // constructor
-    L.Util.setOptions(this, options);
-  },
-  onAdd: function (map) {
-    // happens after added to map
-    var container = L.DomUtil.create('div', 'leaflet-control-github');
-    var $github = $('.map__control--github');
-    if ($github.length){
-      $github.remove();
-      $(container).append($github);
+    options: {
+        position: 'topright'
+    },
+    initialize: function(options) {
+        // constructor
+        L.Util.setOptions(this, options);
+    },
+    onAdd: function(map) {
+        // happens after added to map
+        var container = L.DomUtil.create('div', 'leaflet-control-github');
+        var $github = $('.map__control--github');
+
+        // github button already in markup
+        if ($github.length) {
+            $github.remove();
+            $(container).append($github);
+        // create new button
+        } else {
+            L.DomUtil.create('a', 'btn btn--github', container).setAttribute('href', 'https://github.com/czmj/cyclespoons');
+        }
+        return container;
     }
-    else {
-      L.DomUtil.create('a', 'btn btn--github', container).setAttribute('href', 'https://github.com/czmj/cyclespoons');
-    }
-    return container;
-  }
 });
 
 L.control.github = function(id, options) {
-  return new L.Control.GitHub(id, options);
+    return new L.Control.GitHub(id, options);
 }
--- app/assets/javascripts/application-head/leaflet-control-strava.js
+++ app/assets/javascripts/application-head/leaflet-control-strava.js
@@ -1,64 +1,76 @@
-
 L.Control.Strava = L.Control.extend({
-options: {
-    position: 'topright'
-  },
-  initialize: function (options) {
-    // constructor
-    L.Util.setOptions(this, options);
-    self.routesLayer = new L.LayerGroup();
-  },
-  onAdd: function (map) {
-    // happens after added to map
-    var container = L.DomUtil.create('div', 'leaflet-control-strava');
-    var $strava = $('.map__control--strava');
-    if ($strava.length){
-      $strava.remove();
-      $(container).append($strava);
+    options: {
+        position: 'topright'
+    },
+    initialize: function(options) {
+        // constructor
+        L.Util.setOptions(this, options);
+    },
+    onAdd: function(map) {
+        // happens after added to map
+        var container = L.DomUtil.create('div', 'leaflet-control-strava');
+        var $strava = $('.map__control--strava');
+        this.routesLayer = new L.FeatureGroup();
 
-      $strava.on('click', this.onClick);
-    }
-    else {
-      L.DomUtil.create('a', 'btn btn--strava', container).setAttribute('href', 'https://www.strava.com/oauth/authorize?client_id=19372&response_type=code&redirect_uri=http://cyclespoons.herokuapp.com');
-    }
-    return container;
-  },
-  onClick: function(event) {
-    var $target = $(event.target),
-        $link = $target.closest('.map__control--strava a'),
-        encodedRoutes = $link.attr('data-routes');
-
-    if (encodedRoutes) {
-      event.preventDefault();
-
-      if (map.hasLayer(self.routesLayer)){
-        map.removeLayer(self.routesLayer);
-      }
-      else {
-        encodedRoutes = JSON.parse(encodedRoutes);
-        self.routesLayer.clearLayers();
-  
-        for (let encoded of encodedRoutes) {
-          var coordinates = L.Polyline.fromEncoded(encoded).getLatLngs();
-            
-          var route = L.polyline(
-              coordinates,
-              {
-                  color: 'blue',
-                  weight: 5,
-                  opacity: .7,
-                  lineJoin: 'round'
-              }
-          )
-
-          self.routesLayer.addLayer(route);
+        // strava button in markup
+        if ($strava.length) {
+            $strava.remove();
+            $(container).append($strava);
+            L.DomEvent.addListener($strava[0], 'click', this.onClick, this)
         }
-        map.addLayer(self.routesLayer);
-      }
+
+        return container;
+    },
+    onClick: function(event) {
+        var $target = $(event.target),
+            $link = $target.is('.map__control--strava a') ? $target : $target.closest('.map__control--strava a'),
+            encodedRoutes = $link.attr('data-routes');
+        
+        // data attribute exists 
+        if (encodedRoutes) {
+            L.DomEvent.preventDefault(event);
+            encodedRoutes = JSON.parse(encodedRoutes);
+
+            if (encodedRoutes.length){
+                this.toggleRoutes(encodedRoutes);
+            }
+            // user has no routes to show
+            else {
+                alert('You must add at least one route to Strava to show your routes');
+            }
+        }
+    },
+    toggleRoutes: function(encodedRoutes) {
+        if (!encodedRoutes) {
+            return;
+        }
+
+        // routes are currently shown
+        if (map.hasLayer(this.routesLayer)) {
+            map.removeLayer(this.routesLayer);
+        }
+        // routes are currently hidden
+        else {
+            this.routesLayer.clearLayers();
+            for (let encoded of encodedRoutes) {
+
+                var coordinates = L.Polyline.fromEncoded(encoded).getLatLngs();
+
+                var route = L.polyline(coordinates, {
+                    color: 'blue',
+                    weight: 5,
+                    opacity: .7,
+                    lineJoin: 'round'
+                })
+
+                this.routesLayer.addLayer(route);
+            }
+            map.addLayer(this.routesLayer);
+            map.fitBounds(this.routesLayer.getBounds());
+        }
     }
-  }
 });
 
 L.control.strava = function(id, options) {
-  return new L.Control.Strava(id, options);
+    return new L.Control.Strava(id, options);
 }
